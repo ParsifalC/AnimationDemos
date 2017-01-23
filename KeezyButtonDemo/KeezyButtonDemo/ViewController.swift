35decbcc4f6bcea107091fa4729795b2d3c1047f
Parsifal <zmw@izmw.me>
2017-01-23 22:11:18 +0800

    Keezy animations demo.

--- KeezyButtonDemo/KeezyButtonDemo/ViewController.swift
+++ KeezyButtonDemo/KeezyButtonDemo/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rollBtn: CPScalableButton!
+    @IBOutlet weak var touchedBtn: CPScalableButton!
+    let pathRadius:CGFloat = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // demo for the keezy button
     }
-
+    
+    @IBAction func animate(_ sender: UIButton) {
+        let arcPath = UIBezierPath()
+        arcPath.addArc(withCenter: CGPoint(x: rollBtn.center.x, y:rollBtn.center.y-pathRadius),
+                       radius: pathRadius,
+                       startAngle: CGFloat(M_PI_2),
+                       endAngle: CGFloat(1.5*M_PI),
+                       clockwise: true)
+        
+        let postionAnimation = CAKeyframeAnimation.init(keyPath: "position")
+        postionAnimation.path = arcPath.cgPath
+        postionAnimation.calculationMode = kCAAnimationCubic
+        postionAnimation.timingFunction = nil
+        
+        let rotationAnimation = CABasicAnimation.init(keyPath: "transform.rotation.z")
+        rotationAnimation.fromValue = 0
+        rotationAnimation.toValue = M_PI
+        
+        let scaleAnimation = CABasicAnimation.init(keyPath: "transform.scale")
+        scaleAnimation.fromValue = 1.0
+        scaleAnimation.toValue = 10
+        
+        let opacityAnimation = CABasicAnimation.init(keyPath: "opacity")
+        opacityAnimation.fromValue = 0.1
+        opacityAnimation.toValue = 1.0
+        
+        let groupAniamtion = CAAnimationGroup()
+        groupAniamtion.duration = 1
+        groupAniamtion.repeatCount = 1
+        groupAniamtion.autoreverses = false
+        groupAniamtion.isRemovedOnCompletion = false
+        groupAniamtion.fillMode = kCAFillModeForwards
+        groupAniamtion.animations = [postionAnimation, rotationAnimation, scaleAnimation, opacityAnimation]
+        self.rollBtn.layer.add(groupAniamtion, forKey: nil)
+    }
 }
 
